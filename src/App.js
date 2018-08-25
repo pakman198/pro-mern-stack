06b633cb83247f44a12f3cc7ccfcc1f1c1efeefc
José Francisco Herrera Hernández <pakman_198@hotmail.com>
2018-08-25 12:52:05 -0500

    Passing Data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,68 @@
+class IssueFilter extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the Issue Filter.</div>
+        );
+    }
+}
+
+class IssueRow extends React.Component {
+    render() {
+        const borderedStyle = { border: "1px solid silver", padding: 4 };
+        const {issue_id, issue_title} = this.props;
+
+        return (
+            <tr>
+                <td style={borderedStyle}>{issue_id}</td>
+                <td style={borderedStyle}>{issue_title}</td>
+            </tr>
+        );
+    }
+}
+
+class IssueTable extends React.Component {
+    render() {
+        const borderedStyle = { border: "1px solid silver", padding: 6 };
+
+        return (
+            <table style={ {borderCollapse: "collapse"} }>
+                <thead>
+                    <tr>
+                        <th style={borderedStyle}>Id</th>
+                        <th style={borderedStyle}>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <IssueRow issue_id={1}
+                        issue_title="Error in console when clicking Add" />
+                    <IssueRow issue_id={2}
+                        issue_title="Missing bottom border on panel" />
+                </tbody>
+            </table>
+        );
+    }
+}
+
+class IssueAdd extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the Issue Add entry form.</div>
+        );
+    }
+}
+
 class IssueList extends React.Component {
     render() {
         return (
-            <div>This is a placeholder for the issue list.</div>
-        )
+            <div>
+                <h1>TIssue Tracker</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable />
+                <hr />
+                <IssueAdd />
+            </div>
+        );
     }
 }
 
