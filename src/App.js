0880f7a88b6a9809e09c0e88463682a67d1e9ca2
José Francisco Herrera Hernández <pakman_198@hotmail.com>
2018-08-25 12:14:49 -0500

    React Classes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-const message = continents.map(c => `Hello ${c}!`).join(' ');
-const component = <p>{message}</p>;
+class IssueList extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the issue list.</div>
+        )
+    }
+}
 
 ReactDOM.render(
-component,
+<IssueList />,
 document.getElementById('root')
 );
