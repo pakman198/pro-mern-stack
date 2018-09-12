9889ad68a54ccc7ad0d2c8977528da02e7131bb2
José Francisco Herrera Hernández <pakman_198@hotmail.com>
2018-09-12 16:12:49 -0500

    Writing to MongoDB

--- server.js
+++ server.js
@@ -8,27 +8,6 @@ app.use(express.static('static'));
 app.set('json spaces', 2); // makes the json response look pretty 
 app.use(bodyParser.json());
 
-const issues = [
-    {
-        id: 1,
-        status: 'Open',
-        owner: 'Ravan',
-        created: new Date('2016-08-15'),
-        effort: 5,
-        completionDate: undefined,
-        title: 'Error in console when clicking Add'
-    },
-    {
-        id: 2,
-        status: 'Assigned',
-        owner: 'Eddie',
-        created: new Date('2016-08-16'),
-        effort: 14,
-        completionDate: new Date('2016-08-30'),
-        title: 'Missing bottom border on panel'
-    }
-];
-
 const validIssueStatus = {
     New: true,
     Open: true,
@@ -39,7 +18,6 @@ const validIssueStatus = {
 };
 
 const issueFieldType = {
-    id: 'required',
     status: 'required',
     owner: 'required',
     effort: 'optional',
@@ -78,7 +56,6 @@ app.get('/api/issues', (req, res) => {
 
 app.post('/api/issues', (req, res) => {
     const newIssue = req.body;
-    newIssue.id = issues.length + 1;
     newIssue.created = new Date();
 
     if (!newIssue.status) {
@@ -89,10 +66,17 @@ app.post('/api/issues', (req, res) => {
     
     if (err) {
         res.status(422).json({ message: `Invalid request: ${err}` });
+        return;
     }
 
-    issues.push(newIssue);
-    res.json(newIssue);
+    db.collection('issues').insertOne(newIssue).then(result => 
+        db.collection('issues').find({ _id: result.insertedId }).limit(1).next()
+    ).then(newIssue => {
+        res.json(newIssue);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ message: `Invalid request: ${err}` });
+    });
 });
 
 let db;
