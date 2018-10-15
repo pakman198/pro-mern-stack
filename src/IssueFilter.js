10bc757193ebd7ac9db18b5a0129f093c0b31ad7
José Francisco Herrera Hernández <pakman_198@hotmail.com>
2018-10-15 17:13:17 -0500

    Forms. Grid-Based Forms

--- src/IssueFilter.js
+++ src/IssueFilter.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  Col,
+  Row,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  InputGroup,
+  ButtonToolbar,
+  Button
+} from 'react-bootstrap';
 
 class IssueFilter extends React.Component {
   constructor(props){
@@ -96,17 +106,25 @@ class IssueFilter extends React.Component {
   }
 
   renderSelect() {
-    const { status } = this.state
+    const { status } = this.state;
+
     return (
-      <select value={status} onChange={this.handleStatusChange}>
-        <option value="">(Any)</option>
-        <option value="New">New</option>
-        <option value="Open">Open</option>
-        <option value="Assigned">Assigned</option>
-        <option value="Fixed">Fixed</option>
-        <option value="Verified">Verified</option>
-        <option value="Closed">Closed</option>
-      </select>
+      <FormGroup>
+        <ControlLabel>Status</ControlLabel>
+        <FormControl
+          componentClass="select"
+          value={status}
+          onChange={this.handleStatusChange}
+        >
+          <option value="">(Any)</option>
+          <option value="New">New</option>
+          <option value="Open">Open</option>
+          <option value="Assigned">Assigned</option>
+          <option value="Fixed">Fixed</option>
+          <option value="Verified">Verified</option>
+          <option value="Closed">Closed</option>
+        </FormControl>
+      </FormGroup>
     );
   }
 
@@ -114,17 +132,44 @@ class IssueFilter extends React.Component {
     const { effort_gte, effort_lte, changed } = this.state;
     const status = this.renderSelect();
     return (
-      <div>
-        Status:
-        { status }
-        Effort between: 
-        <input size={5} value={effort_gte} onChange={this.handleEffortGteChange} />
-        -
-        <input size={5} value={effort_lte} onChange={this.handleEffortLteChange} />
-        <button type="button" onClick={this.applyFilter}>Apply</button>
-        <button type="button" onClick={this.resetFilter} disabled={!changed}>Reset</button>
-        <button type="button" onClick={this.clearFilter}>Clear</button>
-      </div>
+      <Row>
+        <Col xs={6} sm={4} md={3}>
+          { status }
+        </Col>
+
+        <Col xs={6} sm={4} md={3}>
+          <FormGroup>
+            <ControlLabel>Effort</ControlLabel>
+            <InputGroup>
+              <FormControl value={effort_gte} onChange={this.handleEffortGteChange} />
+              <InputGroup.Addon>-</InputGroup.Addon>
+              <FormControl value={effort_lte} onChange={this.handleEffortLteChange} />
+            </InputGroup>
+          </FormGroup>
+        </Col>
+
+        <Col xs={12} sm={4} md={3}>
+          <FormGroup>
+            <ControlLabel>&nbsp;</ControlLabel>
+            <ButtonToolbar>
+              <Button bsStyle="primary" onClick={this.applyFilter}>Apply</Button>
+              <Button onClick={this.resetFilter} disabled={!changed}>Reset</Button>
+              <Button onClick={this.clearFilter}>Clear</Button>
+            </ButtonToolbar>
+          </FormGroup>
+        </Col>
+      </Row>
+      // <div>
+      //   Status:
+      //   { status }
+      //   Effort between: 
+      //   <input size={5} value={effort_gte} onChange={this.handleEffortGteChange} />
+      //   -
+      //   <input size={5} value={effort_lte} onChange={this.handleEffortLteChange} />
+      //   <button type="button" onClick={this.applyFilter}>Apply</button>
+      //   <button type="button" onClick={this.resetFilter} disabled={!changed}>Reset</button>
+      //   <button type="button" onClick={this.clearFilter}>Clear</button>
+      // </div>
     );
   }
 }
